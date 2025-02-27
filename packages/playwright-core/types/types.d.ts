ba11c9ca04b9ea9c9850dd63c9dd2699155af187
Michael Mac-Vicar <mmacvicar@gmail.com>
2025-02-27 14:03:54 -0300

    Update declarations

--- packages/playwright-core/types/types.d.ts
+++ packages/playwright-core/types/types.d.ts
@@ -10218,7 +10218,8 @@ export interface Browser {
      */
     clientCertificates?: Array<{
       /**
-       * Exact origin that the certificate is valid for. Origin includes `https` protocol, a hostname and optionally a port.
+       * Exact origin or chromium enterprise policy style URL pattern that the certificate is valid for. Origin includes
+       * `https` protocol, a hostname and optionally a port.
        */
       origin: string;
 
@@ -15590,7 +15591,8 @@ export interface BrowserType<Unused = {}> {
      */
     clientCertificates?: Array<{
       /**
-       * Exact origin that the certificate is valid for. Origin includes `https` protocol, a hostname and optionally a port.
+       * Exact origin or chromium enterprise policy style URL pattern that the certificate is valid for. Origin includes
+       * `https` protocol, a hostname and optionally a port.
        */
       origin: string;
 
@@ -17292,7 +17294,8 @@ export interface APIRequest {
      */
     clientCertificates?: Array<{
       /**
-       * Exact origin that the certificate is valid for. Origin includes `https` protocol, a hostname and optionally a port.
+       * Exact origin or chromium enterprise policy style URL pattern that the certificate is valid for. Origin includes
+       * `https` protocol, a hostname and optionally a port.
        */
       origin: string;
 
@@ -23630,7 +23633,8 @@ export interface BrowserContextOptions {
    */
   clientCertificates?: Array<{
     /**
-     * Exact origin that the certificate is valid for. Origin includes `https` protocol, a hostname and optionally a port.
+     * Exact origin or chromium enterprise policy style URL pattern that the certificate is valid for. Origin includes
+     * `https` protocol, a hostname and optionally a port.
      */
     origin: string;
 
